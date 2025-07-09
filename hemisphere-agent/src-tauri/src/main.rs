d2bc9526a747004a3231a6503b12005c58ed954c
taiki.yoshinaka <taiki.yoshinaka@agc.com>
2025-07-09 04:54:40 +0000

    feat: Upgrade to Tauri v2 for better Windows compatibility
    - Update all Tauri dependencies to v2
    - Rewrite main.rs to use Tauri v2 API (tray icons, menus)
    - Update tauri.conf.json to v2 format
    - Update React imports for Tauri v2 API
    - Add tauri-plugin-shell for shell commands
    - Use new window management API
    
    This should resolve WebView2 build issues on Windows

--- hemisphere-agent/src-tauri/src/main.rs
+++ hemisphere-agent/src-tauri/src/main.rs
@@ -1,11 +1,18 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use anyhow::Result;
-use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri::{
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}, 
+    Manager, Runtime,
+};
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 // Tauriコマンド: ウィンドウをドラッグ開始
 #[tauri::command]
-fn start_dragging(window: tauri::Window) {
+fn start_dragging<R: Runtime>(window: tauri::Window<R>) {
     window.start_dragging().ok();
 }
 
@@ -21,50 +28,61 @@ fn main() -> Result<()> {
 
     info!("Starting Hemisphere Agent");
 
-    // システムトレイメニューの作成
-    let quit = CustomMenuItem::new("quit".to_string(), "終了");
-    let hide = CustomMenuItem::new("hide".to_string(), "隠す");
-    let show = CustomMenuItem::new("show".to_string(), "表示");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(show)
-        .add_item(hide)
-        .add_native_item(tauri::SystemTrayMenuItem::Separator)
-        .add_item(quit);
-
-    let system_tray = SystemTray::new().with_menu(tray_menu);
-
     tauri::Builder::default()
-        .system_tray(system_tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::LeftClick {
-                position: _,
-                size: _,
-                ..
-            } => {
-                let window = app.get_window("main").unwrap();
-                if window.is_visible().unwrap() {
-                    window.hide().unwrap();
-                } else {
-                    window.show().unwrap();
-                    window.set_focus().unwrap();
-                }
-            }
-            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "quit" => {
-                    std::process::exit(0);
-                }
-                "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    window.hide().unwrap();
-                }
-                "show" => {
-                    let window = app.get_window("main").unwrap();
-                    window.show().unwrap();
-                    window.set_focus().unwrap();
-                }
-                _ => {}
-            },
-            _ => {}
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            // メニューアイテムの作成
+            let quit_i = MenuItemBuilder::with_id("quit", "終了").build(app)?;
+            let hide_i = MenuItemBuilder::with_id("hide", "隠す").build(app)?;
+            let show_i = MenuItemBuilder::with_id("show", "表示").build(app)?;
+            
+            // メニューの作成
+            let menu = MenuBuilder::new(app)
+                .items(&[&show_i, &hide_i, &quit_i])
+                .build()?;
+            
+            // システムトレイアイコンの作成
+            let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .on_menu_event(move |app, event| match event.id.as_ref() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    "hide" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.hide().unwrap();
+                        }
+                    }
+                    "show" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let visible = window.is_visible().unwrap_or(false);
+                            if visible {
+                                window.hide().unwrap();
+                            } else {
+                                window.show().unwrap();
+                                window.set_focus().unwrap();
+                            }
+                        }
+                    }
+                })
+                .build(app)?;
+            
+            Ok(())
         })
         .invoke_handler(tauri::generate_handler![start_dragging])
         .run(tauri::generate_context!())
